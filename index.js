0683a95ea3c6fdf1d5008dd98b00887b4b5aa983
iv.mogilevskiy <iv.mogilevskiy@tensor.ru>
2017-04-06 17:22:34 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-var TelegramBot = require('node-telegram-bot-api');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var Entities = require('html-entities').XmlEntities;
- 
-entities = new Entities();
-
-    // ”станавливаем токен, который выдавал нам бот.
-    var token = '370885878:AAHNT9nRTHMd6MJ8dQvRbzw9GFpzomt719s';
-	var port = process.env.PORT || 8443;
-	var host = process.env.HOST;
-    // ¬ключить опрос сервера
-    var bot = new TelegramBot(token, {polling: true, webhook: {
+const
+   TelegramBot = require('node-telegram-bot-api'),
+   XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest,
+   Entities = require('html-entities').XmlEntities,
+   entities = new Entities(),
+   token = '370885878:AAHNT9nRTHMd6MJ8dQvRbzw9GFpzomt719s',
+   port = process.env.PORT || 8443,
+   host = process.env.HOST,
+   busToWork = 'http://yartr.ru/rasp.php?vt=1&nmar=78&q=0&id=47&view=1',
+   busToHome = 'http://yartr.ru/rasp.php?vt=1&nmar=78&q=1&id=424&view=1',
+   bot = new TelegramBot(token, {
+   polling: true, webhook: {
       "port": port,
       "host": host
-    }});
-	
-	bot.onText(/\/start/, (msg, match) => {
-		const chatId = msg.chat.id;
-		bot.sendMessage(chatId, 'Hello! Commands: /gohome - bus on station "Hospital", /gowork bus on station "Prospect Tolbuhina"');
-	});
-	
-	bot.onText(/\/gohome/, (msg, match) => {
-		const chatId = msg.chat.id;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://yartr.ru/rasp.php?vt=1&nmar=78&q=1&id=424&view=1', false);
-		xhr.send();
-		var 
-			original = entities.decode(xhr.responseText).replace(/<[^>]+>/g,' ');
-			original = original.substring(40, original.length - 15);
-		bot.sendMessage(chatId, msg.chat.first_name + ', ' + original);
-	});
+   }
+});
 
-    bot.onText(/\/gowork/, (msg, match) => {
-		const chatId = msg.chat.id;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://yartr.ru/rasp.php?vt=1&nmar=78&q=0&id=47&view=1', false);
-		xhr.send();
-		var 
-			original = entities.decode(xhr.responseText).replace(/<[^>]+>/g,' ');
-			original = original.replace(/назад/g, '');
-			var position = original.indexOf('Ав 78');
-			var pos = original.indexOf('Ав', position + 2);
-			if (pos + 1) {
-				original = original.substring(position, pos);
-			} else {
-				original = original.substring(position, original.lenght);
-			}
-		bot.sendMessage(chatId, msg.chat.first_name + ', ' + original);
+function prepareText(link) {
+   let
+      xhr = new XMLHttpRequest(),
+      original, position, pos;
+
+   xhr.open('GET', link, false);
+   xhr.send();
+   original = entities.decode(xhr.responseText).replace(/<[^>]+>/g, ' ').replace(/назад/g, '');
+   position = original.indexOf('Ав 78');
+   pos = original.indexOf('Ав', position + 2);
+   if (pos + 1) {
+      original = original.substring(position, pos);
+   } else {
+      original = original.substring(position, original.length);
+   }
+   return original;
+}
+
+bot.onText(/\/start/, (msg) => {
+   const chatId = msg.chat.id;
+   bot.sendMessage(chatId, 'Hello! Commands: /gohome - bus on station "Hospital", /gowork bus on station "Prospect Tolbuhina"');
+});
+
+bot.onText(/\/gohome/, (msg) => {
+   bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(busToHome));
+});
+
+bot.onText(/\/gowork/, (msg) => {
+   bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(busToWork));
 });

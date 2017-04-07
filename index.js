5c9e5402c09eec9e11fe44a947e5cdf70fb44579
iv.mogilevskiy <iv.mogilevskiy@tensor.ru>
2017-04-07 19:11:07 +0300

    BusMiniJson

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const
     TelegramBot = require('node-telegram-bot-api'),
     bus = require('./bus.json'),
+    busMin = require('./bus.min.json'),
     XMLHttpRequests = require('xmlhttprequest').XMLHttpRequest,
     Entities = require('html-entities').XmlEntities,
     entities = new Entities(),
@@ -14,12 +15,18 @@ const
          }
     });
 
-function prepareText(link) {
+function createLink(str) {
+   let
+      array = str.split();
+
+   return 'http://yartr.ru/rasp.php?vt=1&nmar='+ 78 +'&q='+ array[0] + '&id=' + array[1] + '&view=1';
+}
+
+function prepareText(str) {
     let
-        xhr = new XMLHttpRequests(),
-        original, position, pos;
-
-
+       xhr = new XMLHttpRequests(),
+       link= createLink(str),
+       original, position, pos;
     xhr.open('GET', link, false);
     xhr.send();
     original = entities.decode(xhr.responseText).replace(/<[^>]+>/g, ' ').replace(/назад/g, '');
@@ -33,24 +40,20 @@ function prepareText(link) {
     return original;
 }
 
-function createLink(bus, way, station) {
-   return 'http://yartr.ru/rasp.php?vt=1&nmar='+ bus +'&q='+ way + '&id=' + station + '&view=1';
-}
-
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Hello! Commands: /gohome - bus on station "Hospital", /gowork bus on station "Prospect Tolbuhina"');
 });
 
 bot.onText(/\/gohome/, (msg) => {
-    bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(createLink(78, 0, 424)));
+    bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(createLink('1 424')));
 });
 
 bot.onText(/\/gowork/, (msg) => {
-    bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(createLink(78, 0, 47)));
+    bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(createLink('0 47')));
 });
 
 bot.onText(/\/test (.+) (.+)/, (msg, match) => {
-    bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(bus['78'][match[1]][match[2]]));
+    bot.sendMessage(msg.chat.id, msg.chat.first_name + ', ' + prepareText(busMin['78'][match[1]][match[2]]));
 });
 
 bot.onText(/\/help/, (msg) => {
